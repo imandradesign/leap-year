310714fbaa72a785668c55eeea4565125c770ace
Connie Rodriguez and Imandra McKenzie <imandra@>
2019-03-04 19:00:25 -0800

    Finish Jquery and JavaScript todetermine if year is a leap year and update styles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
+ var leapYear = function(yearInput) {
+  if ((yearInput % 4 === 0) && (yearInput % 100 !== 0) || (yearInput % 400 === 0))
+   {
+    return "This is a leap year!"
+    } else {
+    return "This is not a leap year."
+    }
+  }
+
+
 $(document).ready(function(){
+  $("#answer").hide();
 
   $(".container form").submit(function(event){
     var yearInput = parseInt($("input#year").val());
+    var testing = leapYear(yearInput)
     event.preventDefault();
-    $("#result").text(yearInput);
+    $("#result").text(testing);
+    $("#answer").show();
   })
 });
